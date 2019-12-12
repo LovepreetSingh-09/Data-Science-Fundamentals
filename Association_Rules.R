b1a112c45c302132cc57c6ddedc9d762b1f251df
LovepreetSingh-09 <54183912+LovepreetSingh-09@users.noreply.github.com>
2019-12-12 19:56:34 +0530

    Update Association_Rules.R

--- Association_Rules.R
+++ Association_Rules.R
@@ -5,4 +5,8 @@ bookbaskets <- read.transactions("bookdata.tsv.gz", format="single",
                                  sep="\t",cols=c("userid", "title"),rm.duplicates=T, header = T)
 summary(bookbaskets)
 
+class(bookbaskets)
+bookbaskets
 
+colnames(bookbaskets)[1:10]
+rownames(bookbaskets)[1:10]
